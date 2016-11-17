64f8f6cb719ee2e586d6304289d78c1d12e25c40
BjoernP94 <bjoern_94@hotmail.com>
2016-11-17 14:42:23 +0100

    Oprettelse af falsk blodtryk
    Brugt random generator til at opstille et falsk blodtryk og sende det ud
    i en liste

---
+++ DAQ/DAQData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAQ
+{
+    public class DAQData
+    {
+        public List<Double> Blodtryk()
+        {
+            Random r = new Random();
+            List<double> Liste = new List<double>();
+            int counter = 0;
+
+            while (!Console.KeyAvailable)
+            {
+                double random = r.Next(0, 10000);
+                Liste.Add(random);
+                //Console.WriteLine(Liste[counter]);
+                counter++;
+            }
+            return Liste;
+        }
+    }
+}
